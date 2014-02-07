4227b49a1b0cea27eb72fc2968fdedeb99d2322e
Changbeom Ahn <kewlbear@gmail.com>
2014-02-07 09:22:44 +0900

    Initial Commit

---
+++ MailCharSet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MailCharSet
+//
+//  Created by 안창범 on 2014. 2. 7..
+//  Copyright (c) 2014년 Changbeom Ahn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MailCharSetTests/MailCharSetTests.m
@@ -0,0 +1,34 @@
+//
+//  MailCharSetTests.m
+//  MailCharSetTests
+//
+//  Created by 안창범 on 2014. 2. 7..
+//  Copyright (c) 2014년 Changbeom Ahn. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MailCharSetTests : XCTestCase
+
+@end
+
+@implementation MailCharSetTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
